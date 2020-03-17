5438235e6eef2a3163ef472d88865a5a1de1cea4
Thomas <thomas@craftacademy.se>
2020-03-17 13:21:53 +0100

    adds method to change the message by clicking a button

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 const App = props => {
+
+	const changeMessage = () => {
+		props.dispatch({ type: '', payload: 'Hello from a function' })
+	}
 	return (
-		<h1>{props.message}</h1>
+		<>
+			<h1>{props.message}</h1>
+			<button
+				onClick={changeMessage}
+			>Change message
+			</button>
+		</>
 	)
 }
 
--- src/state/reducers/rootReducer.js
+++ src/state/reducers/rootReducer.js
@@ -1,7 +1,10 @@
 import initialState from '../store/initialState'
 
-const rootReducer = (state = initialState) => {
-	return state
+const rootReducer = (state = initialState, action) => {
+	return {
+		...state,
+		message: action.payload || initialState.message
+	}
 }
 
 export default rootReducer
