1fa8540ead275c72157bb4c96c6ee0e7fa97b203
Thomas <thomas@craftacademy.se>
2020-03-17 10:26:05 +0100

    displays app state message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class App extends Component {
 	state = {
 		message: "Hello World from component state"
 	}
 	render() {
+
 		return (
 			<>
-				<h1>{this.state.message}</h1>
+				<h1>{this.props.state.message}</h1>
 			</>
 		);
 	}
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		state: state
+	}
+}
+
+export default connect(mapStateToProps)(App);
