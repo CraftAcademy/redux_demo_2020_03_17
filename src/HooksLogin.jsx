80ce146728c2f575ec6f39144044fe843070df00
Thomas <thomas@craftacademy.se>
2020-03-21 18:29:26 +0100

    small refactiorings and code formatting

--- src/HooksLogin.jsx
+++ src/HooksLogin.jsx
@@ -3,34 +3,24 @@ import { onLogin, onLogout } from './modules/auth'
 import { useSelector, useDispatch } from 'react-redux'
 
 const HooksLogin = () => {
-	const dispatch = useDispatch()
-	const authenticated = useSelector(state => state.auth.authenticated)
-	const userEmail = useSelector(state => state.auth.userEmail)
+  const dispatch = useDispatch()
+  const authenticated = useSelector(state => state.auth.authenticated)
 
-
-	let login
-	if (authenticated) {
-		login = (
-			<>
-				<button onClick={event => onLogout(event, dispatch)}>Logout</button>
-			</>
-		)
-	} else {
-		login = (
-			<form onSubmit={event => onLogin(event, dispatch)}>
-				<input name="email" placeholder="Email" />
-				<input name="password" type="password" placeholder="Password" />
-				<button type="submit">
-					Login using hooks
-          </button>
-			</form>
-		)
-	}
-	return (
-		<>
-			{login}
-		</>
-	)
+  let login
+  if (authenticated) {
+    login =
+      <button onClick={() => onLogout(dispatch)}>
+        Logout
+      </button>
+  } else {
+    login =
+      <form onSubmit={event => onLogin(event, dispatch)}>
+        <input name="email" placeholder="Email" />
+        <input name="password" type="password" placeholder="Password" />
+        <input type="submit" value="Login using hooks" />
+      </form>
+  }
+  return login
 }
 
 export default HooksLogin
